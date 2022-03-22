c8a3d628e1102401e7524f9eaafe0e569783acf1
Vitaliy Voloshyn <vitaliy.srt1985@gmail.com>
2022-03-22 13:06:59 +0200

    lesson1

---
+++ task2.js
@@ -0,0 +1,10 @@
+/*Объявить две переменные: admin и name. Записать в name строку "Василий"; Скопировать
+значение из name в admin. Вывести admin (должно вывестись «Василий»).
+*/
+console.log('Задание2\n')
+var admin = ''
+var name = ''
+
+name = 'Василий'
+admin = name
+console.log(admin)
